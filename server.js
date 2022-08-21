325af76900049df3118ca27ecd7f5df4c474c2cc
Wizi8914 <loick.konieczny@gmail.com>
2022-08-21 20:46:02 +0200

    add new butons

--- server.js
+++ server.js
@@ -3,17 +3,16 @@ const path = require('path');
 
 const app = express();
 
-app.use("/js", express.static(path.resolve(__dirname, "static")));
+//app.use("/js", express.static(path.resolve(__dirname, "static")));
+app.use(express.static(path.join(__dirname, "global")))
 
-app.get("/*", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "index.html"))
 });
 
-app.listen(process.env.PORT || 5500, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server running...")
 })
-
-
 
 app.get("/", (req, res) => {
     res.render("index.js")
